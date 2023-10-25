97be9a260f140b99202fc8a417297384366eacf5
sunny patel <sunnykpatel.skp@gmail.com>
2023-10-25 10:54:54 -0400

    add fetch data logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,42 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import Loading from './Loading';
+
+const url = 'https://course-api.com/react-tours-project';
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
+  const [cards, setCards] = useState([]);
+
+  const fetchCards = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      if (!response.ok) {
+        throw new Error('error in fetch url');
+      }
+      const data = await response.json();
+      setCards(data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
+
+  console.log(cards);
+
+  useEffect(() => {
+    fetchCards();
+  }, []);
+
+  if (isLoading) {
+    return (
+      <>
+        <Loading />
+      </>
+    );
+  }
+
   return (
     <>
       <h1>hi</h1>
